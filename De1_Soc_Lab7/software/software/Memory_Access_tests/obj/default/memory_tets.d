de67d4b7484c1b81f237ad7d0b66f63e26fe8d14
AntonB0g <avb6403@rit.edu>
2022-11-15 11:04:32 -0500

    cretaed a C test

---
+++ De1_Soc_Lab7/software/software/Memory_Access_tests/obj/default/memory_tets.d
@@ -0,0 +1,12 @@
+obj/default/memory_tets.o: memory_tets.c \
+ ../Memory_Access_tests_bsp//HAL/inc/io.h \
+ ../Memory_Access_tests_bsp//HAL/inc/alt_types.h system.h \
+ ../Memory_Access_tests_bsp/linker.h
+
+../Memory_Access_tests_bsp//HAL/inc/io.h:
+
+../Memory_Access_tests_bsp//HAL/inc/alt_types.h:
+
+system.h:
+
+../Memory_Access_tests_bsp/linker.h:
