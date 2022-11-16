d9ef36697cbb60d469e40730ef4935c91f86defb
AntonB0g <avb6403@rit.edu>
2022-11-16 13:24:45 -0500

    Added ISR and JTAG msg support to memory_test.c

--- De1_Soc_Lab7/src/memory_tets.c
+++ De1_Soc_Lab7/src/memory_tets.c
@@ -1,6 +1,7 @@
 #include "io.h"
 #include <stdio.h>
 #include "system.h"
+#include <stdbool.h>
 
 // create standard embedded type definitions
 typedef   signed char   sint8;              // signed 8 bit values
@@ -15,7 +16,7 @@ typedef         float   real32;
 volatile uint32* RAMPtr = (volatile uint32*) INFERRED_RAM_BE_0_BASE;
 volatile uint8* LedPtr = (volatile uint8*) LEDS_BASE;
 volatile uint32* KeyPtr = (volatile uint32*) PUSHBUTTONS_BASE;
-volatile uint32* JTAG_Ptr = (volatile uint32*) JTAG_UART_0_BASE
+volatile uint32* JTAG_Ptr = (volatile uint32*) JTAG_UART_0_BASE;
 
 uint32 BYTE_test_data = 0x00000000; // actual data is 0x00
 uint32 HALF_test_data = 0x00001234;// actual data is 0x1234
@@ -26,6 +27,7 @@ uint16 half_data = 0x0000;
 uint32 halfs = 0x00000000;
 uint32 words = 0x00000000;
 const char str[17]= "RAM Test is Done!";
+bool Test = true;
 
 
 uint32 start_address =  0x00000000;
@@ -107,21 +109,20 @@ void RAM_test(uint32 s_address, uint32 ram_size, uint32 test_data, uint32 access
 		}
 }
 
-void JTAG_msg_sender(const char* str, uint8 size)
+void JTAG_msg_sender( const char *str, uint8 length)
 {
-	for(auto ch : *str)
+	for (uint8 i = 0; i < length; i++)
 	{
-		*JTAG_Ptr = ch;
+		*(JTAG_Ptr) = str[i];
 	}
 }
 
 void ISR()
 {
-	if(*(KeyPtr+3)== 0x01)
-	{
-		*LedPtr = 0b010101010;
 
-	}
+	Test = false;
+	*LedPtr = 0b010101010;
+	JTAG_msg_sender(str, 17);
 	*(KeyPtr + 3) = 0; // EdgeCapture reset
 }
 
@@ -130,7 +131,7 @@ int main()
 	*LedPtr = 0x00;
 	alt_ic_isr_register(PUSHBUTTONS_IRQ_INTERRUPT_CONTROLLER_ID,PUSHBUTTONS_IRQ_TYPE,ISR,0,0);
 	*(KeyPtr+2) = 0x2; //set the interrupt mask for KEY1
-	while(1){
+	while(Test){
 		RAM_test(start_address, 16384, BYTE_test_data, 8);
 		*LedPtr = 0x00;
 		RAM_test(start_address, 16384, HALF_test_data, 16);
