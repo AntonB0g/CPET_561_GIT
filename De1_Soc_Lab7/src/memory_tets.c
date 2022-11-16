b9fbd4eab02731a39aabd828d51b110a3442d56a
AntonB0g <avb6403@rit.edu>
2022-11-16 11:57:08 -0500

    Added software test for Mem Access

--- De1_Soc_Lab7/src/memory_tets.c
+++ De1_Soc_Lab7/src/memory_tets.c
@@ -12,12 +12,21 @@ typedef unsigned long   uint32;             // unsigned 32 bit values
 typedef         float   real32;
 
 
-volatile uint32* RAMPtr = (volatile uint32*) RAM_INFR_CUSTOM_IP_0_BASE;
+volatile uint32* RAMPtr = (volatile uint32*) INFERRED_RAM_BE_0_BASE;
 volatile uint8* LedPtr = (volatile uint8*) LEDS_BASE;
+volatile uint32* KeyPtr = (volatile uint32*) PUSHBUTTONS_BASE;
+volatile uint32* JTAG_Ptr = (volatile uint32*) JTAG_UART_0_BASE
 
 uint32 BYTE_test_data = 0x00000000; // actual data is 0x00
 uint32 HALF_test_data = 0x00001234;// actual data is 0x1234
 uint32 WORD_test_data = 0xABCDEF90;// already actual data
+uint8  byte_data = 0x00;
+uint32 byte_num = 0x00000000;
+uint16 half_data = 0x0000;
+uint32 halfs = 0x00000000;
+uint32 words = 0x00000000;
+const char str[17]= "RAM Test is Done!";
+
 
 uint32 start_address =  0x00000000;
 
@@ -25,8 +34,8 @@ void RAM_test(uint32 s_address, uint32 ram_size, uint32 test_data, uint32 access
 {
 	switch(access_size){
 	case 8:
-			uint8 byte_data = uint8(test_data);
-			uint32 byte_num = ram_size; // number 32 bit WORDS is 512 in 16 Bytes of data
+			byte_data = (uint8) test_data;
+			byte_num = ram_size; // number 32 bit WORDS is 512 in 16 Bytes of data
 
 			for (uint32 i = byte_num -1; i > 0; i--)
 			{
@@ -49,8 +58,8 @@ void RAM_test(uint32 s_address, uint32 ram_size, uint32 test_data, uint32 access
 			}
 			break;
 	case 16:
-			(uint16) half_data = (uint16) test_data);
-			uint32 halfs = ram_size/2; // number 32 bit WORDS is 512 in 16 Bytes of data
+			half_data = (uint16) test_data;
+			halfs = ram_size/2; // number 32 bit WORDS is 512 in 16 Bytes of data
 
 			for (uint32 i = halfs -1; i > 0; i--)
 			{
@@ -73,7 +82,7 @@ void RAM_test(uint32 s_address, uint32 ram_size, uint32 test_data, uint32 access
 			}
 			break;
 	case 32:
-			uint32 words = ram_size/4; // number 32 bit WORDS is 512 in 16 Bytes of data
+			words = ram_size/4; // number 32 bit WORDS is 512 in 16 Bytes of data
 
 			for (uint32 i = words -1; i > 0; i--)
 			{
@@ -98,9 +107,29 @@ void RAM_test(uint32 s_address, uint32 ram_size, uint32 test_data, uint32 access
 		}
 }
 
+void JTAG_msg_sender(const char* str, uint8 size)
+{
+	for(auto ch : *str)
+	{
+		*JTAG_Ptr = ch;
+	}
+}
+
+void ISR()
+{
+	if(*(KeyPtr+3)== 0x01)
+	{
+		*LedPtr = 0b010101010;
+
+	}
+	*(KeyPtr + 3) = 0; // EdgeCapture reset
+}
+
 int main()
 {
 	*LedPtr = 0x00;
+	alt_ic_isr_register(PUSHBUTTONS_IRQ_INTERRUPT_CONTROLLER_ID,PUSHBUTTONS_IRQ_TYPE,ISR,0,0);
+	*(KeyPtr+2) = 0x2; //set the interrupt mask for KEY1
 	while(1){
 		RAM_test(start_address, 16384, BYTE_test_data, 8);
 		*LedPtr = 0x00;
