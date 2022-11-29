c032900e56e8b78d859d401a460219d9ccaaa13e
AntonB0g <avb6403@rit.edu>
2022-11-29 09:34:00 -0500

    Demo 8 - done. Demo 9 - started.

---
+++ demo8/hw/software/Arbitration_demo/obj/default/Arbitration_part1.d
@@ -0,0 +1,18 @@
+obj/default/Arbitration_part1.o: Arbitration_part1.c system.h \
+ ../Arbitration_demo_bsp/linker.h \
+ ../Arbitration_demo_bsp//HAL/inc/sys/alt_irq.h \
+ ../Arbitration_demo_bsp//HAL/inc/nios2.h \
+ ../Arbitration_demo_bsp//HAL/inc/alt_types.h \
+ ../Arbitration_demo_bsp/system.h
+
+system.h:
+
+../Arbitration_demo_bsp/linker.h:
+
+../Arbitration_demo_bsp//HAL/inc/sys/alt_irq.h:
+
+../Arbitration_demo_bsp//HAL/inc/nios2.h:
+
+../Arbitration_demo_bsp//HAL/inc/alt_types.h:
+
+../Arbitration_demo_bsp/system.h:
